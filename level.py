e3dd5424d60820f23f96855bfd1b3e609f136a46
gitmskhl <gglmskhl@gmail.com>
2026-04-08 22:40:11 +0300

    исправлена проблема приближения и отдаления

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import utils
 import pygame
 import pytmx
 import player
+import math
 
 
 class Level:
@@ -10,19 +11,38 @@ class Level:
     MAX_ZOOM_LEVEL = 5
 
     def __init__(self):
-        self.background = utils.loadimg('images/Maps/World.png', 1)
-        self.backgroundorig = self.background
+        self.backgroundorig = utils.loadimg('images/Maps/World.png', 1).convert_alpha()
+        self.background = self.backgroundorig
         self.scale = 1.0
         self.zoom_level = 0
         self.xcamera = 0.0
         self.ycamera = 0.0
 
     def render(self, screen):
-        self.clamp_camera(screen.get_size())
-        screen.blit(
-            self.background,
-            [-round(self.xcamera * self.scale), -round(self.ycamera * self.scale)],
-        )
+        viewport_size = screen.get_size()
+        self.clamp_camera(viewport_size)
+
+        map_width = self.backgroundorig.get_width()
+        map_height = self.backgroundorig.get_height()
+        visible_width = min(map_width, max(1, math.ceil(viewport_size[0] / self.scale) + 2))
+        visible_height = min(map_height, max(1, math.ceil(viewport_size[1] / self.scale) + 2))
+        source_x = min(max(int(self.xcamera), 0), max(0, map_width - visible_width))
+        source_y = min(max(int(self.ycamera), 0), max(0, map_height - visible_height))
+        offset_x = -round((self.xcamera - source_x) * self.scale)
+        offset_y = -round((self.ycamera - source_y) * self.scale)
+        visible_rect = pygame.Rect(source_x, source_y, visible_width, visible_height)
+        visible_part = self.backgroundorig.subsurface(visible_rect)
+
+        if self.scale == 1:
+            screen.blit(visible_part, [offset_x, offset_y])
+            return
+
+        scaled_size = [
+            max(1, round(visible_width * self.scale)),
+            max(1, round(visible_height * self.scale)),
+        ]
+        scaled_visible_part = pygame.transform.scale(visible_part, scaled_size)
+        screen.blit(scaled_visible_part, [offset_x, offset_y])
 
     def screen_to_world(self, x, y):
         return x / self.scale + self.xcamera, y / self.scale + self.ycamera
@@ -50,9 +70,6 @@ class Level:
         anchor_world_x, anchor_world_y = self.screen_to_world(*anchor_pos)
         self.zoom_level = target_zoom_level
         self.scale = self.ZOOM_FACTOR ** self.zoom_level
-        width = max(1, round(self.backgroundorig.get_width() * self.scale))
-        height = max(1, round(self.backgroundorig.get_height() * self.scale))
-        self.background = pygame.transform.scale(self.backgroundorig, [width, height])
         self.xcamera = anchor_world_x - anchor_pos[0] / self.scale
         self.ycamera = anchor_world_y - anchor_pos[1] / self.scale
         return True
